b7e718f67024e497acf0f87f848c2a8a2c77f49e
Zachary Davison <thingsdoer@gmail.com>
2025-06-08 09:54:31 +0200

    Refactored path simplify/beautify logic out to Path class.

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useRef, useState } from "react"
-import * as perfect from "perfect-freehand"
-import type { Point, Path } from "./schema/path"
-import { getSvgPathFromStroke } from "./drawing/path-tools"
-import { Simplify } from "simplify-ts"
+import { Path } from "./drawing/path"
 
 function Canvas() {
 
@@ -11,24 +8,21 @@ function Canvas() {
   const [currentPath, setCurrentPath] = useState<Path | null>(null)
 
   const onMouseDown = (e: React.MouseEvent) => {
-    const path: Path = { points: [[e.pageX, e.pageY]] }
+    const path = new Path([[e.pageX, e.pageY]])
     setCurrentPath(path)
     console.log('onMouseDown', currentPath)
   }
 
   const onMouseMove = (e: React.MouseEvent) => {
     if (!currentPath) { return }
-    setCurrentPath({
-      points: [...currentPath.points, [e.pageX, e.pageY]]
-    })
+    setCurrentPath(currentPath.appended([e.pageX, e.pageY]))
     console.log('onMouseMove', currentPath)
   }
 
   const onMouseUp = (e: React.MouseEvent) => {
     if(!currentPath) { return }
 
-    const simplifiedPoints = Simplify([...currentPath.points, [e.pageX, e.pageY]].map(p => ({ x: p[0], y:p[1] }))).map(p => [p.x, p.y] as Point)
-    paths.push({ points: simplifiedPoints })
+    paths.push(currentPath.simplified())
     setPaths(paths)
     setCurrentPath(null)
   }
@@ -49,12 +43,8 @@ function Canvas() {
       paths.push(currentPath)
     }
 
-    console.log('re-rendering paths', paths)
-
     for (const path of paths) {
-      const stroke = perfect.getStroke(path.points) as Point[]
-      const svg = getSvgPathFromStroke(stroke)
-      const path2d = new Path2D(svg)
+      const path2d = new Path2D(path.beautified().renderToSVGPath())
       ctx.fillStyle = '#000000'
       ctx.fill(path2d)
     }
---
+++ src/drawing/path.ts
@@ -0,0 +1,116 @@
+import { Simplify } from "simplify-ts"
+import * as perfect from "perfect-freehand"
+
+/**
+ * A 2D point in space.
+ */
+export type Point = [x: number, y: number]
+
+/**
+ * An SVG path string.
+ *
+ * Can be used to construct a Path2D object.
+ *
+ * @see https://css-tricks.com/svg-path-syntax-illustrated-guide/
+ */
+export type SVGPathString = string
+
+/**
+ * - raw: A set of raw points that haven't been modified in any way.
+ * - simplified: A simplified set of points, most suitable for storage.
+ * - beautified: A complex, 'filled' path of points, generated from perfect-freehand, most suitable for rendering to look good.
+ */
+export type PathType = 'raw' | 'simplified' | 'beautified'
+
+const average = (a: number, b: number) => (a + b) / 2
+
+/**
+ * A path in zac.ooo.
+ * 
+ * Has utility functions for rendering different versions of itself for different usages.
+ * 
+ * All functions return a new path, and do not modify in place.
+ */
+export class Path {
+  public readonly points: Point[]
+  public type: PathType
+
+  constructor(points: Point[] = [], type: PathType = 'raw')  {
+    this.points = points
+    this.type = type
+  }
+
+  /**
+   * Create a new path by appending `point`.
+   * @param point
+   * @returns A new path object with the point appended.
+   */
+  appended(point: Point): Path {
+    return new Path(
+      [...this.points, point],
+      this.type
+    )
+  }
+
+  /**
+   * Uses `simplify-ts` to simplify the path.
+   * @returns A new, simplified path.
+   */
+  simplified(): Path {
+    return new Path(
+      Simplify(this.points.map(p => ({x: p[0], y: p[1]}))).map(p => [p.x, p.y]), // Convert points between formats and back.
+      'simplified'
+    )
+  }
+
+  /**
+   * Uses perfect-freehand to simulate pressure and create an outline, for a more realistic looking path.
+   * 
+   * Note: The resulting path should be filled, not stroked.
+   * 
+   * @returns A new, beautified path.
+   */
+  beautified(): Path {
+    return new Path(
+      perfect.getStroke(this.points) as Point[],
+      'beautified'
+    )
+  }
+
+  /**
+   * Renders an SVG path string suitable for drawing in HTML Canvas or as an <svg/> element.
+   * @returns A string representing the path as SVG.
+   */
+  renderToSVGPath(): SVGPathString {
+    const len = this.points.length
+
+    if (len < 4) {
+      return ''
+    }
+  
+    let a = this.points[0]
+    let b = this.points[1]
+    const c = this.points[2]
+  
+    let result = `M${a[0].toFixed(2)},${a[1].toFixed(2)} Q${b[0].toFixed(
+      2
+    )},${b[1].toFixed(2)} ${average(b[0], c[0]).toFixed(2)},${average(
+      b[1],
+      c[1]
+    ).toFixed(2)} T`
+  
+    for (let i = 2, max = len - 1; i < max; i++) {
+      a = this.points[i]
+      b = this.points[i + 1]
+      result += `${average(a[0], b[0]).toFixed(2)},${average(a[1], b[1]).toFixed(
+        2
+      )} `
+    }
+  
+    if (closed) {
+      result += 'Z'
+    }
+  
+    return result
+  }
+}
